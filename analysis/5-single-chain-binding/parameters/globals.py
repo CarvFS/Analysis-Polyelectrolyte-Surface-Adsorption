27568a3ce322ea251d04eb43e5b4b0553c42e208
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-04-04 20:14:28 -0700

    update simulation directory and parameters

--- analysis/5-single-chain-binding/parameters/globals.py
+++ analysis/5-single-chain-binding/parameters/globals.py
@@ -11,11 +11,11 @@ from pathlib import Path
 
 # MDAnalysis trajectory parameters
 START: int = int(25e3)  # First frame to read
-STOP: int = int(250e3)  # Last frame to read
-STEP: int = 10  # Step between frames to read
+STOP: int = int(500e3)  # Last frame to read
+STEP: int = 5  # Step between frames to read
 MODULE: str = "joblib"  # parallel processing {joblib, multiprocessing, dask}
-N_JOBS: int = 30  # Number of parallel jobs
-N_BLOCKS: int = 180  # Number of blocks to split trajectory into
+N_JOBS: int = 32  # Number of parallel jobs
+N_BLOCKS: int = 160  # Number of blocks to split trajectory into
 SOLVENT: bool = True  # Whether or not to include solvent in the analysis
 
 # Data processing parameters
